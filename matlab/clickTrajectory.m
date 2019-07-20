5e0c37a6e9fed9f56cfd598d8f9831989eb1b8f1
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2019-07-19 23:44:58 -0400

    support aux variables in clickTrajectory

--- matlab/clickTrajectory.m
+++ matlab/clickTrajectory.m
@@ -1,4 +1,4 @@
-function clickTrajectory(clickAxis,~,clo,ivp,pin,y0in,tspan,parIx,varIx,tracjectoryFigID,nClick,markerColor)
+function clickTrajectory(clickAxis,~,clo,pin,y0in,tspan,parIx,vars,tracjectoryFigID,nClick,markerColor)
 %A mouse click inside the axes will trigger a parameter
 %selection crosshair. A second click selects a parameter
 %combination to simulate, and resulting simulation is displayed
@@ -7,7 +7,7 @@ function clickTrajectory(clickAxis,~,clo,ivp,pin,y0in,tspan,parIx,varIx,tracject
 %could find closest solved instance, get y0 for beginning of
 %feature detection interval
 
-if ~exist('varIx','var'), varIx=1; end
+if ~exist('vars','var'), vars=clo.prob.varNames(1); end
 if ~exist('nClick','var'), nClick=1; end
 if nClick>6, error('I refuse to do more than 6 clicks'); end
 
@@ -17,8 +17,22 @@ if ~exist('markerColor','var'), markerColor='k';end
 % p=repmat([ivp.par(:).value],nClick,1);
 y0=repmat(y0in(:)',nClick,1);
 p=repmat(pin(:)',nClick,1);
-vNames={ivp.var(:).name};
-pNames={ivp.par(:).name};
+
+varIsAux=false(size(vars));
+for v=1:length(vars) 
+    tmpIx=find(clo.prob.varNames==string(vars{v})); 
+    if isempty(tmpIx)
+        tmpIx=find(clo.prob.auxNames==string(vars{v}));
+        if isempty(tmpIx)
+            error(['unknown variable: ' vars(v)]);
+        else
+            varIsAux(v)=true;
+            varIx(v)=tmpIx;
+        end 
+    else
+        varIx(v)=tmpIx;
+    end
+end
 
 markers='osdp^v';
 
@@ -44,58 +58,75 @@ hf=figure(tracjectoryFigID);
 hf.KeyPressFcn=@keypress;
 
 clf
-[X,T]=integrate();
-plotTrajectories(X,T);
+[X,T,AUX]=integrate();
+plotTrajectories(X,T,AUX);
 
-    function [X,T,nStored]=integrate()
+    function [X,T,AUX]=integrate()
         clo.trajectory();
         xx=clo.getX();
-        %         AUX=clo.getAux();
+        aux=clo.getAux();
         tt=clo.getT()*clo.tscale;
         nStored=clo.getNstored();
         for i=1:nClick
             T{i}=tt(1:nStored(i),i);
             X{i}=xx(1:nStored(i),:,i);
+            AUX{i}=aux(1:nStored(i),:,i);
         end
     end
 
     function keypress(src,evt)
         switch(evt.Key)
             case 'g'
-                [X,T]=integrate();
-                plotTrajectories(X,T);
+                [X,T,AUX]=integrate();
+                plotTrajectories(X,T,AUX);
                 
             case 'c'
-                [xx,tt]=integrate();
+                [xx,tt,aux]=integrate();
                 for i=1:nClick
                     T{i}=[T{i};tt{i}+T{i}(end)];
                     X{i}=[X{i};xx{i}];
+                    AUX{i}=[AUX{i};aux{i}];
                 end
-                plotTrajectories(X,T);
+                plotTrajectories(X,T,AUX);
         end
     end
 
-    function plotTrajectories(X,T)
+    function plotTrajectories(X,T,AUX)
         
         for i=1:nClick
             
             t=T{i};
-            x=X{i};
             
             %Time plot
             ax=subplot(nClick,1,i);
             if numel(varIx)==1
+                
+                if varIsAux
+                    x=AUX{i};
+                    vNames=clo.prob.auxNames;
+                else
+                    x=X{i};
+                    vNames=clo.prob.varNames;
+                end
+                
                 plot(t,x(:,varIx),'k');
                 xlim([t(1),t(end)]);
                 ylabel(vNames{varIx});
                 YLIM=ylim();
-                xmax=max(x(:,varIx(1)));
-                xmin=min(x(:,varIx(1)));
+                xmax=max(x(:,varIx));
+                xmin=min(x(:,varIx));
                 ylim([min(YLIM(1),xmin-0.01*abs(xmin)), max(YLIM(2),xmax+0.01*abs(xmax))]);
                 
             elseif numel(varIx)==2
                 %Time plot
                 yyaxis left
+                if varIsAux(1)
+                    x=AUX{i};
+                    vNames=clo.prob.auxNames;
+                else
+                    x=X{i};
+                    vNames=clo.prob.varNames;
+                end
                 plot(t,x(:,varIx(1)));
                 xlim([t(1),t(end)]);
                 YLIM=ylim();
@@ -106,6 +137,13 @@ plotTrajectories(X,T);
                 ylabel(vNames{varIx(1)});
                 
                 yyaxis right
+                if varIsAux(2)
+                    x=AUX{i};
+                    vNames=clo.prob.auxNames;
+                else
+                    x=X{i};
+                    vNames=clo.prob.varNames;
+                end
                 plot(t,x(:,varIx(2)));
                 xlim([t(1),t(end)]);
                 YLIM=ylim();
@@ -126,7 +164,7 @@ plotTrajectories(X,T);
             XLIM=xlim();
             
             line((XLIM(1)+XLIM(end))*0.02,YLIM(2)*0.9,'marker',markers(i),'linestyle','none','color',markerColor,'linewidth',1,'tag','marks');
-            title([pNames{parIx(1)} '=' num2str(p(i,parIx(1)),2) ', ' pNames{parIx(2)} '=' num2str(p(i,parIx(2)),2)]);
+            title([clo.prob.parNames{parIx(1)} '=' num2str(p(i,parIx(1)),2) ', ' clo.prob.parNames{parIx(2)} '=' num2str(p(i,parIx(2)),2)]);
         end
     end
 end
