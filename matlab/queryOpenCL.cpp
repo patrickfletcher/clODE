7ff435553175df4179be4be2d0e430a03d6c5e42
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2019-09-10 16:34:18 -0400

    Create queryOpenCL.cpp

---
+++ matlab/queryOpenCL.cpp
@@ -0,0 +1,20 @@
+//MEX function to use OpenCLResource class to query opencl devices and return info about them
+
+#include "mex.h"
+#include "OpenCLResource.hpp"
+		   
+/* Input Arguments */
+#define	T_IN	prhs[0]
+
+/* Output Arguments */
+#define	YP_OUT	plhs[0]
+
+void mexFunction( int nlhs, mxArray *plhs[], 
+		  int nrhs, const mxArray*prhs[] )
+     
+{ 
+
+
+    return;
+    
+}
