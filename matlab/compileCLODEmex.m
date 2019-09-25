10f7e1f7aac1515435740e9cbe894d05b5214981
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2019-09-25 11:27:21 -0700

    Update compileCLODEmex.m

--- matlab/compileCLODEmex.m
+++ matlab/compileCLODEmex.m
@@ -4,14 +4,19 @@ clear
 % %%%%%%%%%%%%%%%% CONFIGURATION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if ismac
     % Code to run on Mac plaform
+    opencl_include_dir = '';
+    opencl_lib_dir = '';
+    libopencl='-framework OpenCL';
 elseif isunix
     % Code to run on Linux plaform
     opencl_include_dir = '/usr/local/cuda-7.5/targets/x86_64-linux/include';
     opencl_lib_dir = '/usr/lib64/nvidia';
+    libopencl='-lOpenCL';
 elseif ispc
     % Code to run on Windows platform 
     opencl_include_dir = 'C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/include';
     opencl_lib_dir = 'C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1/lib/x64';
+    libopencl='-lOpenCL';
 else
     disp('Cannot recognize platform')
 end
@@ -28,14 +33,14 @@ debugchar='';
 
 mex('queryOpenCL.cpp',[clode_path,'OpenCLResource.cpp'],...
     debugchar,...
-    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], '-lOpenCL' );
+    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], libopencl );
 
 
 %% CLODE
 mex('clODEmex.cpp',[clode_path,'OpenCLResource.cpp'],[clode_path,'CLODE.cpp'],...
     debugchar,...
     ['-DCLODE_ROOT=\"' clode_path '\"'],...
-    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], '-lOpenCL' );
+    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], libopencl );
 
 %%
 
@@ -43,7 +48,7 @@ mex('clODEtrajectorymex.cpp',[clode_path,'OpenCLResource.cpp'],[clode_path,'CLOD
     [clode_path,'CLODEtrajectory.cpp'],...
     debugchar,...
     ['-DCLODE_ROOT=\"' clode_path '\"'],...
-    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], '-lOpenCL' );
+    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], libopencl );
 
 %%
 
@@ -51,4 +56,4 @@ mex('clODEfeaturesmex.cpp',[clode_path,'OpenCLResource.cpp'],[clode_path,'CLODE.
     [clode_path,'CLODEfeatures.cpp'],...
     debugchar,...
     ['-DCLODE_ROOT=\"' clode_path '\"'],...
-    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], '-lOpenCL' );
+    ['-I' clode_path], ['-I' opencl_include_dir], ['-L' opencl_lib_dir], libopencl );
