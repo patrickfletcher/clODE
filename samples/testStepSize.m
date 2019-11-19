bc6a09e14e2b1fdc7761dc22aaba431596663865
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2019-11-19 14:11:10 -0500

    WIP implement better stepsize controller
    -including the stepsize ratio still not working

--- samples/testStepSize.m
+++ samples/testStepSize.m
@@ -3,8 +3,9 @@ clear
 
 odefile='lactotroph.ode';
 precision='single';
-clo=clODEtrajectory(odefile,precision);
+clo=clODE(odefile,precision);
 % clo.stepper='rk4'; %default='dopri5'
+% clo.selectDevice(); %{'type','gpu'}, {'vendor','nvidia'}, {platID,devID}, 'maxComputeUnits','maxClock' 
 
 %solver parameters
 sp=clODE.solverParams();%create required ODE solver parameter struct
@@ -14,7 +15,7 @@ sp=clODE.solverParams();%create required ODE solver parameter struct
 % sp.reltol=1e-3;
 % sp.max_steps=10000000;
 
-tspan=[0,3000];
+tspan=[0,1000];
 
 %set up parameters and initial conditions
 nPts=32;
@@ -28,38 +29,15 @@ P=repmat(p,nPts,1);
 clo.initialize(tspan, X0, P, sp);
 clo.seedRNG(42)
 clo.transient(); %warm up the GPU for timing
-%%
+
+%run the simulation
 tic
-clo.trajectory();
+clo.transient();
 toc
 
+%read data back from the GPU
 tic
-t=clo.getT();
-x=clo.getX();
-dx=clo.getDx();
-aux=clo.getAux();
-nSteps=clo.getNsteps();
-nStored=clo.getNstored();
+nextTspan=clo.getTspan;
+xf=clo.getXf;
+auxf=clo.getAuxf;
 toc
-
-
-%%
-figure(1)
-tix=32;
-vix=1;
-thisNstore=nStored(tix)
-tt=t(1:thisNstore,tix);
-xx=x(1:thisNstore,vix,tix);
-auxx=aux(1:thisNstore,tix);
-plot(tt,xx(:,1))
-xlabel('t')
-ylabel(clo.prob.varNames(vix))
-
-%% show dt(t)
-figure(2)
-tix=32;
-vix=1;
-tt=t(1:thisNstore,tix);
-plot(tt(1:end-1),diff(tt))
-xlabel('t')
-ylabel('dt')
