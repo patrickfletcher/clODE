2dbff17b6da2716052c16065946fc59796634df6
Wolf Byttner <wolf@byttner.org>
2023-03-18 16:48:59 +0000

    Add tensorstore attribution

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The TensorStore Authors
+# Copyright 2020 The CLODE/TensorStore Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Derived from Tensorstore's setup.py
+# https://github.com/google/tensorstore/
+
 """setuptools script for installing the Python package.
 
 This invokes bazel via the included `bazelisk.py` wrapper script.
