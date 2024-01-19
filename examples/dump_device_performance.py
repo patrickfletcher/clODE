a50141312d63ecdf5b89739284f95d9e25f70397
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2024-01-19 13:16:26 -0500

    report median instead of mean

--- examples/dump_device_performance.py
+++ examples/dump_device_performance.py
@@ -37,25 +37,32 @@ def test_lorenz_rk4(platform_id, device_id, num_pts, reps):
 
 
     t_average = []
-    print(f"Time for 1000 RK4 steps of the Lorenz system. Average of {reps} repetitions.\n(N, time)")
+    print(f"Time for 1000 RK4 steps of the Lorenz system. {reps} repetitions.\n(N, min, median, max time)")
     for num in num_pts:
-        tsum = 0.0
+        times = []
         Pars = np.tile(default_parameters, (num, 1))
         X0 = np.tile(initial_state, (num, 1))
         integrator.set_problem_data(X0, Pars)
 
-        #warm-up pass
-        integrator.transient(update_x0=False)
+        #warm-up passes - seems to make output more reliable
+        for _ in range(5):
+            integrator.set_problem_data(X0, Pars)
+            integrator.transient(update_x0=False)
+            integrator.get_final_state()
 
         for _ in range(reps):
             t0 = time.perf_counter()
             # integrator.initialize(X0, Pars) #include host->device transfers?
             integrator.transient(update_x0=False)
             # integrator.get_final_state() #include device->host transfers?
-            tsum += time.perf_counter() - t0
+            times.append(time.perf_counter() - t0)
 
-        print(f"{num}\t{tsum/reps:0.4} s")
-        t_average.append(tsum/reps)
+        t_mean = sum(times)/reps
+        t_min = min(times)
+        t_max = max(times)
+        t_median = np.median(times)
+        print(f"{num}\t {t_min:.3g}, {t_median:.3g}, {t_max:.3g} s")
+        t_average.append(t_median)
 
     return t_average
     
@@ -67,10 +74,10 @@ if __name__ == "__main__":
     clode.set_log_level(clode.runtime.DEFAULT_LOG_LEVEL) 
 
     ocl_info = clode.runtime.query_opencl()
-    # ocl_info = ocl_info[:3]
+    ocl_info = ocl_info[:3]
 
-    num_pts = [int(2**n) for n in np.arange(0,20)]
-    reps = 30
+    num_pts = [int(2**n) for n in np.arange(0,18)]
+    reps = 50
     
     device_names = []
     for i, ocl in enumerate(ocl_info):
